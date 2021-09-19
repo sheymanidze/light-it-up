876622ed153e763dda3b0e1e72d373b7f73e081b
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-09-19 11:29:36 -0400

    setting up canvas for a game

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react';
+
+//Material UI components
+import Box from '@material-ui/core/Box';
+
+import './game.css'
+
+import logo from '../Home/img/logo.png'
 
 const Game = () => {
+
+  //canvas
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+
+
+    // setting up canvas
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 500;
+  }, [])
+
   return (
-    <div>
+    <div className="flex-center-column" id="game">
+      <Box className="logoGame">
+        <img className="logo" src={logo} alt="logo"></img>
+      </Box>
+
+      <Box className="borderBox">
+        <canvas
+          className="canvas1"
+          id="canvas"
+          ref={canvasRef}>
+        </canvas>
+      </Box>
 
     </div>
   )
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from 'react';
 
 import './home.css'
 
-//Material UI
+//Material UI Components
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import Box from '@material-ui/core/Box';
