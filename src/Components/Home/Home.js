f9ee565be4d609a47facb5f43ef4ba7174724f45
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-09-19 10:56:05 -0400

    custom button created

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,13 +6,41 @@ import './home.css'
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 //images
 import logo from './img/logo.png'
 import HomePageImage from './img/boo.PNG'
 
+
+
+//Custom made button
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, yellow 30%, orange 90%)',
+    borderRadius: 50,
+    border: 0,
+    color: 'white',
+    fontSize: 40,
+    width: 300,
+    height: 90,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 255, 0, .3)',
+    marginBottom: 25,
+
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+});
+
 const Home = () => {
+
+  //button
+  const classes = useStyles();
+
+
   return (
     <div className="homePage flex-center-column">
 
@@ -28,14 +56,8 @@ const Home = () => {
       <Box>
         <img className="homePageImg" src={HomePageImage} alt="Ghost"></img>
       </Box>
-
-
-
-
-
-
       <Link className="links" to="/game">
-        <Button className="startBtn">
+        <Button className="startBtn" classes={{ root: classes.root, label: classes.label }}>
           Let's Start
         </Button>
       </Link>
